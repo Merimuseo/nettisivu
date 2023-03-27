f43cbd5651d9164c1d505c7ddeac4226236a6727
Merimuseo <129053133+Merimuseo@users.noreply.github.com>
2023-03-27 17:03:15 +0300

    Add files via upload

---
+++ tiedot.js
@@ -0,0 +1,6 @@
+var pvm = "22.5 - 25.8.2044";
+/* var vuosi = "2023"; */
+var vuosi = pvm.slice(pvm.length - 4);
+
+var postiosoite = "Satulatie , 65230 Vaasa";
+var postadress = "Sadelvägen , 65230 Vasa";
